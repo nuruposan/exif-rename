f508198ee1e1dd880c7e75564dd411ff1b470acc
nuruposan <8068623+nuruposan@users.noreply.github.com>
2025-10-18 16:16:38 +0900

    Changed various behavior
    - add destination directory option
    - add bash value to destination filename

--- gen-exif-rename-sh.sh
+++ gen-exif-rename-sh.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-function abort() {
+function onAbort() {
   echo -e "\naborted!" >&2
   exit 2
 }
 
-function checkenv() {
+function checkEnv() {
   type "exiftool" >/dev/null 2>&1; EQ=$?
   type "jq" >/dev/null 2>&1; JQ=$?
 
@@ -15,59 +15,68 @@ function checkenv() {
   fi
 }
 
-function jq2tsv() {
+jq2tsv() {
   jq '.[0]' \
-    | jq -r '["mv",."SourceFile",."File:Directory",."EXIF:CreateDate",."Composite:ImageSize",."EXIF:Model"]|@tsv'
+    | jq -r '[."EXIF:CreateDate",."Composite:ImageSize",."EXIF:Model"]|@tsv'
 }
 
-function tsv2cmd() {
-  sed -e "s/\t/ \"/" \
-    -e "s/\t/\" \"/" \
-    -e "s/\t/\//" \
-    -e "s/\([0-9]\+\):\([0-9]\+\):\([0-9]\+\) /\1\2\3-/" \
+function tsv2dst() {
+  sed -e "s/\([0-9]\+\):\([0-9]\+\):\([0-9]\+\) /\1\2\3-/" \
     -e "s/\([0-9]\+\):\([0-9]\+\):\([0-9]\+\)\t/\1\2\3_/" \
     -e "s/\t/_/" \
-    -e "s/$/.jpg\"/"
+    -e "s/\t/_/" \
+    -e "s/ /-/"
 }
 
-trap 'abort' INT
+# verify that the required software is installed
+checkEnv
 
-checkenv
+# set Ctrl-C to call onAbort()
+trap 'onAbort' INT
 
 SRC_DIR="$1"
-if [ $# -ne 1 ] || [ ! -d "$SRC_DIR" ]; then
+DST_DIR=`echo $2 | sed -e 's/\/$//'`
+
+if [ $# -ne 2 ] || [ ! -d "${SRC_DIR}" ] || [ ! -d "${DST_DIR}" ]; then
   {
     echo "usage:"
-    echo "  $0 <TARGET_DIR> | tee <OUTPUT_SCRIPT>"
-    echo "  bash <OUTPUT_SCRIPT>"
+    echo "  $0 SRC_DIR DEST_DIR | tee SCRIPT"
+    echo "  bash SCRIPT"
   } >&2
 
   exit 1
 fi
 
-FILE_NUM=`find $SRC_DIR | grep -i "jpg$" | wc -l`
+FILE_NUM=`find $SRC_DIR | grep -i -e "\.jpg$" -e "\.jpeg$" | wc -l`
 if [ "$FILE_NUM" -eq "0" ]; then
   echo "No picture file found. Nothing to do!" >&2
   exit 0
 fi
 
-
 echo "#!/bin/bash"
-echo "# target dir: $SRC_DIR"
+echo ""
+echo "# source dir: ${SRC_DIR} (${FILE_NUM} files)"
+echo "# destination dir: ${DST_DIR}"
+echo "# generated at: "`date "+%Y%m%d-%H%M%S"`
+echo ""
 
 LOOP_CNT=1
-find $SRC_DIR | grep -i "jpg$" | while read JPEG_FILE; do
-  if exiftool -exif -if '$exif' "$JPEG_FILE" >/dev/null; then
-    echo -n "`exiftool -s -G -j "$JPEG_FILE" | jq2tsv | tsv2cmd`"
+find $SRC_DIR | grep -i -e "\.jpg$" -e "\.jpeg$" | while read SRC_FILE; do
+  echo -ne "\r($LOOP_CNT/$FILE_NUM) " >&2
+
+  CHK_STR=`md5sum "${SRC_FILE}" | cut -c 1-6`
+
+  if exiftool -exif -if '$exif' "$SRC_FILE" >/dev/null; then
+    EXIF_JQ=`exiftool -s -G -j "${SRC_FILE}"`
+    EXIF_TSV=`echo "${EXIF_JQ}" | jq2tsv`
+    DST_FILE=`echo "${EXIF_TSV}" | tsv2dst`"_${CHK_STR}.jpg"
+
+    echo "mv \"${SRC_FILE}\" \"${DST_DIR}/${DST_FILE}\""
   else
-    echo -n "# no EXIF: $JPEG_FILE"
+    echo "# no EXIF: ${SRC_FILE}"
   fi
 
-  echo -n " "
-  echo -ne "($LOOP_CNT/$FILE_NUM)\r" >&2
-  echo ""
-
-  ((LOOP_CNT = $LOOP_CNT + 1))
+  ((LOOP_CNT = ${LOOP_CNT} + 1))
 done
 echo ""
 
